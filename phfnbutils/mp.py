661301fd3377d29194d37ee8d0d6c74430987405
Philippe Faist <phfaist@gmail.com>
2021-06-01 17:00:35 +0200

    added support for specifying list of dictionaries for inputs

--- phfnbutils/mp.py
+++ phfnbutils/mp.py
@@ -13,6 +13,8 @@ import multiprocessing
 import itertools
 import random
 
+import numpy as np  # np.prod
+
 
 
 
@@ -23,6 +25,77 @@ def _streams_init_pool():
 
 
 
+def get_iter_over_all_inputs_via_arglists(args_values_lists):
+    # sanity check:
+    if isinstance(args_values_lists[0][0], dict):
+        raise ValueError("I expected a list or iterables for input arguments, but I got a "
+                         "dictionary. Did you mean to specify the `input=` keyword "
+                         "argument instead?")
+    iterable = itertools.chain.from_iterable(
+        (itertools.product(*args_values_list) for args_values_list in args_values_lists)
+    )
+    total_n = sum(
+        int(np.prod( [len(x) for x in args_values_list] ))
+        for args_values_list in args_values_lists
+    )
+    return total_n, iterable
+
+def _d_val_to_list_of_values(dval):
+    if isinstance(dval, str):
+        # special case for string, because it's iterable but we don't want to
+        # iterate over its characters!!
+        return [dval]
+    try:
+        return list(iter(dval))
+    except TypeError:
+        return [dval]
+
+def get_iter_over_all_inputs_via_dicts(inputs):
+    d_iters = [] # prepare an iterator for each dictionary in inputs
+    total_n = 0
+    for d in inputs:
+        keys, vals = zip(*d.items())
+        valiters = [_d_val_to_list_of_values(val) for val in vals]
+        vals_iter = itertools.product(*valiters)
+        def get_d_iter(keys=keys, vals_iter=vals_iter):
+            # make sure we freeze the value of keys and vals_iters for this
+            # generator
+            return ( dict(zip(keys, v)) for v in vals_iter )
+        d_iters.append( get_d_iter(keys, vals_iter) )
+        total_n += int(np.prod( [len(x) for x in valiters] ))
+        
+    return total_n, itertools.chain(*d_iters)
+
+
+def get_inputs_iterable(*, args_values_lists=None, inputs=None, shuffle_tasks=False):
+    r"""
+    Construct an iterator over all specified input combinations.
+
+    We are given a specification of combinations of input values as expected in
+    `parallel_apply_func_on_input_combinations()`.  This function returns a
+    two-item tuple `(total_n, iterable)`, where `total_n` is the total number of
+    input combinations, and iterable iterates over these input combinations.
+
+    If `shuffle_tasks` is `True`, then the iterable is converted to a list and
+    its order is shuffled.
+    """
+    if args_values_lists:
+        if inputs is not None:
+            raise ValueError(
+                "Cannot specify both input argument lists and `inputs` keyword argument "
+                "to get_inputs_iterable()."
+            )
+        total_n, iterable = get_iter_over_all_inputs_via_arglists(args_values_lists)
+    else:
+        total_n, iterable = get_iter_over_all_inputs_via_dicts(inputs)
+
+    if shuffle_tasks:
+        list_of_inputs = list(iterable)
+        random.shuffle(list_of_inputs) # shuffle in place
+        return total_n, list_of_inputs
+
+    return total_n, iterable
+
 #
 # Tiny utility for applying a function using multiprocessing.Pool across a
 # cartesian product of input arguments, with progress bar
@@ -31,6 +104,7 @@ def parallel_apply_func_on_input_combinations(
         fn,
         *args_values_lists,
         #
+        inputs=None,
         processes=None,
         shuffle_tasks=True,
         sequential_execution=False,
@@ -38,50 +112,64 @@ def parallel_apply_func_on_input_combinations(
 ):
     # Notes: (convert to docstring, TODO)
     #
-    # - fn should accept a single argument, a tuple of arguments
+    # - fn should accept a single argument, a tuple of arguments (if
+    #   `*args_values_lists` is specified) or a dictionary (if `inputs=`) is
+    #   specified.
+    #
+    # - if `inputs` is non-`None`, it should be a list of dictionaries where
+    #   each value itself can be a single value or a list.  For one of these
+    #   dictionaries, an associated list of input values is assembled by taking
+    #   all combinations of the values within the dictionary.  The resulting
+    #   lists are then concatenated to obtain the list of all inputs.
     #
     # - shuffle_tasks shuffles the order of execution of all tasks; time
     #   progress reporting might be more reliable than if all the hard tasks are
     #   at the end of the values lists.  A cost is that the iterable
     #   representing the cartesian product of input arguments must be flattened
     #   to a list, possibly using lots of memory if you have many input
-    #   arguments with many values
+    #   arguments with many values.
     
     from tqdm.auto import tqdm
-    import numpy as np
 
-    args_values_lists = list(args_values_lists)
+    if len(args_values_lists) > 0 and inputs is not None:
+        raise ValueError("Cannot specify both input argument lists "
+                         "and `inputs` keyword argument to "
+                         "parallel_apply_func_on_input_combinations().")
 
-    list_of_inputs = itertools.chain.from_iterable(
-        (itertools.product(*args_values_list) for args_values_list in args_values_lists)
+    total_num_inputs, iterable_of_inputs = get_inputs_iterable(
+        args_values_lists=args_values_lists,
+        inputs=inputs,
+        shuffle_tasks=shuffle_tasks,
     )
-    if shuffle_tasks:
-        list_of_inputs = list(list_of_inputs)
-        random.shuffle(list_of_inputs) # shuffle in place
-        total_num_inputs = len(list_of_inputs)
-    else:
-        total_num_inputs = sum(
-            int(np.prod( [len(x) for x in args_values_list] ))
-            for args_values_list in args_values_lists
-        )
-
 
     # if chunksize is given, and if the function object supports chunked calls,
     # then do that.
     if chunksize is not None and hasattr(fn, 'call_with_inputs'):
-        if not isinstance(list_of_inputs, list):
-            list_of_inputs = list(list_of_inputs)
+        if not isinstance(iterable_of_inputs, list):
+            iterable_of_inputs = list(iterable_of_inputs)
+        #
+        # Instead of calling fn(input), we will call
+        # fn.call_with_inputs([input1, input2, ...]) with all the inputs in a
+        # given chunk.  So what we do is we simply "redefine" what the function
+        # is and what the inputs are.
+        #
+        #  fn                 ->     fn.call_with_inputs
+        #  list_of_inputs     ->     [  [in_chunk1_1, in_chunk1_2, ... ],
+        #                               [in_chunk2_1, in_chunk2_2, ... ], ... ]
+        #
+        #
+
         fn = fn.call_with_inputs
         # chunk inputs into sizes of chunksize
-        list_of_inputs = [ list_of_inputs[i:i+chunksize]
-                           for i in range(0, len(list_of_inputs), chunksize) ]
-        total_num_inputs = len(list_of_inputs)
-        #logger.debug("Chunked inputs, new list of inputs = %r", list_of_inputs)
+        iterable_of_inputs = [ iterable_of_inputs[i:i+chunksize]
+                               for i in range(0, len(iterable_of_inputs), chunksize) ]
+        total_num_inputs = len(iterable_of_inputs)
+        #logger.debug("Chunked inputs, new list of inputs = %r", iterable_of_inputs)
         # and now, reset the 'chunksize' argument to multiprocessing.Pool.imap_unordered()
         chunksize = None
 
     if sequential_execution:
-        for inp in tqdm(list_of_inputs, total=total_num_inputs):
+        for inp in tqdm(iterable_of_inputs, total=total_num_inputs):
             fn(inp)
         return
 
@@ -93,7 +181,7 @@ def parallel_apply_func_on_input_combinations(
 
     with multiprocessing.Pool(**mp_pool_kwargs) as pool:
         for _ in tqdm(
-                pool.imap_unordered( fn, list_of_inputs, **mp_pool_imap_kwargs ),
+                pool.imap_unordered( fn, iterable_of_inputs, **mp_pool_imap_kwargs ),
                 total=total_num_inputs
         ):
             pass
--- test/test_mp.py
+++ test/test_mp.py
@@ -14,7 +14,10 @@ import multiprocessing
 
 
 
-from phfnbutils.mp import parallel_apply_func_on_input_combinations
+from phfnbutils.mp import (
+    parallel_apply_func_on_input_combinations,
+    get_inputs_iterable
+)
 
 # ------------------------------------------------------------------------------
 
@@ -46,8 +49,8 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
             calls_recorder = CallsRecorder(manager.list())
             parallel_apply_func_on_input_combinations(
                 calls_recorder,
-                ['ABC', 'DEF'],
-                ['Z', 'Z'],
+                [['A','B','C'], ['D','E','F']],
+                [['Z'], ['Z']],
                 [[0,1], [10,20]],
                 shuffle_tasks=True,
                 sequential_execution=True,
@@ -100,8 +103,8 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
             calls_recorder = CallsRecorder(manager.list())
             parallel_apply_func_on_input_combinations(
                 calls_recorder,
-                ['ABC', 'DEF'],
-                ['Z', 'Z'],
+                [['A','B','C'], ['D','E','F']],
+                [['Z'], ['Z']],
                 [[0,1], [10,20]],
                 shuffle_tasks=False,
                 sequential_execution=True,
@@ -132,8 +135,8 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
             calls_recorder = CallsRecorder(manager.list())
             parallel_apply_func_on_input_combinations(
                 calls_recorder,
-                ['ABC', 'DEF'],
-                ['Z', 'Z'],
+                [['A','B','C'], ['D','E','F']],
+                [['Z'], ['Z']],
                 [[0,1], [10,20]],
                 shuffle_tasks=True,
             )
@@ -163,8 +166,8 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
             calls_recorder = CallsRecorder(manager.list())
             parallel_apply_func_on_input_combinations(
                 calls_recorder,
-                ['ABC', 'DEF'],
-                ['Z', 'Z'],
+                [['A','B','C'], ['D','E','F']],
+                [['Z'], ['Z']],
                 [[0,1], [10,20]],
                 chunksize=5,
             )
@@ -185,6 +188,38 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
                     (0, 20),
                     (1, 10),
                     (1, 20),
+                ])
+            )
+
+    def test_4(self):
+        
+        inputs = [
+            {'x': ['A', 'B', 'C'],
+             'y': ['D', 'E'],},
+            {'x': ['A', 'B', 'C'],
+             'y': 'FF'},
+        ]
+
+        with multiprocessing.Manager() as manager:
+            calls_recorder = CallsRecorder(manager.list())
+            parallel_apply_func_on_input_combinations(
+                calls_recorder,
+                inputs=inputs,
+            )
+            def _to_sorted_list(X):
+                return sorted(X, key=lambda x: (x['x'], x['y']))
+            self.assertEqual(
+                _to_sorted_list(list(calls_recorder.called_args)),
+                _to_sorted_list([
+                    dict(x='A', y='D'),
+                    dict(x='B', y='D'),
+                    dict(x='C', y='D'),
+                    dict(x='A', y='E'),
+                    dict(x='B', y='E'),
+                    dict(x='C', y='E'),
+                    dict(x='A', y='FF'),
+                    dict(x='B', y='FF'),
+                    dict(x='C', y='FF'),
                 ])
             )
 
@@ -218,6 +253,102 @@ class Test_parallel_apply_func_on_input_combinations(unittest.TestCase):
                                         (8, 32, 4),
                                         (8, 40, 5), ]) )
 
+
+
+class Test_get_inputs_iterable(unittest.TestCase):
+    def test_args_list(self):
+        total_n, iterable = get_inputs_iterable(args_values_lists=[[
+                [1, 2, 3],
+                ['A'],
+                ['alpha', 'beta'],
+            ]])
+        self.assertEqual(
+            list(iterable),
+            [
+                (1, 'A', 'alpha'),
+                (1, 'A', 'beta'),
+                (2, 'A', 'alpha'),
+                (2, 'A', 'beta'),
+                (3, 'A', 'alpha'),
+                (3, 'A', 'beta'),
+            ]
+        )
+        self.assertEqual(total_n, 6)
+
+    def test_args_list_2(self):
+        total_n, iterable = get_inputs_iterable(args_values_lists=[[
+                [1, 2, 3],
+                ['A'],
+                ['alpha', 'beta'],
+            ]], shuffle_tasks=True)
+        self.assertEqual(
+            set(list(iterable)),
+            set([
+                (1, 'A', 'alpha'),
+                (1, 'A', 'beta'),
+                (2, 'A', 'alpha'),
+                (2, 'A', 'beta'),
+                (3, 'A', 'alpha'),
+                (3, 'A', 'beta'),
+            ])
+        )
+        self.assertEqual(total_n, 6)
+
+    def test_dicts_1(self):
+        total_n, iterable = get_inputs_iterable(inputs=[
+            {
+                'x': [1, 2, 3],
+                'y': ['A'],
+                'z': ['alpha', 'beta'],
+            },
+        ])
+        self.assertEqual(
+            list(iterable),
+            [
+                dict(x=1, y='A', z='alpha'),
+                dict(x=1, y='A', z='beta'),
+                dict(x=2, y='A', z='alpha'),
+                dict(x=2, y='A', z='beta'),
+                dict(x=3, y='A', z='alpha'),
+                dict(x=3, y='A', z='beta'),
+            ]
+        )
+        self.assertEqual(total_n, 6)
+
+    def test_dicts_2(self):
+        total_n, iterable = get_inputs_iterable(inputs=[
+            {
+                'x': [1, 2, 3],
+                'y': ['A'],
+                'z': ['alpha', 'beta'],
+            },
+            {
+                'x': 'x',
+                'y': 'y',
+                'z': 'z',
+            },
+            {
+                'x': [1, 2, 3],
+                'y': ['Z'],
+                'z': 'gamma',
+            },
+        ])
+        self.assertEqual(
+            list(iterable),
+            [
+                dict(x=1, y='A', z='alpha'),
+                dict(x=1, y='A', z='beta'),
+                dict(x=2, y='A', z='alpha'),
+                dict(x=2, y='A', z='beta'),
+                dict(x=3, y='A', z='alpha'),
+                dict(x=3, y='A', z='beta'),
+                dict(x='x', y='y', z='z'),
+                dict(x=1, y='Z', z='gamma'),
+                dict(x=2, y='Z', z='gamma'),
+                dict(x=3, y='Z', z='gamma'),
+            ]
+        )
+        self.assertEqual(total_n, 10)
 
 
 
